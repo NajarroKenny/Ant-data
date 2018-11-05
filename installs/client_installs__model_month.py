8082de2522ae0293118a800124bc0a28bc2fa499
Peter Kasprowicz <peter.kasprowicz@gmail.com>
2018-11-05 09:55:17 -0600

    More refactoring

--- installs/client_installs__model_month.py
+++ installs/client_installs__model_month.py
@@ -1,25 +1,26 @@
 from pandas import DataFrame
-from elasticsearch_dsl import Search
+from elasticsearch_dsl import Search, Q
 from ant_data import elastic
 
-def df():
+def search():
     s = Search(using=elastic, index='installs') \
-        .query("term", doctype='install')
-    s.aggs.bucket('models', 'terms', field='model.raw') \
+        .query(Q('term', doctype='install') & ~Q('term', model='Kingo Shopkeeper'))
+    s.aggs.bucket('models', 'terms', field='model') \
         .bucket('by_months', 'date_histogram', field='opened', interval='month')
 
     s = s[:0]
-    response = s.execute()
+    return s.execute()
+
+def df():
+    response = search()
 
     obj = {}
     for model in response.aggregations.models.buckets:
-        obj[model.key] = {}
-        for month in model.by_months.buckets:
-            obj[model.key][month.key_as_string] = month.doc_count
+        obj[model.key] = { month.key_as_string: month.doc_count for month in model.by_months.buckets }
 
     df = DataFrame(obj)
     df = df.reindex(df.index.astype('datetime64'))
-    df = df.drop(['Kingo Shopkeeper'], axis=1)
+
     return df
 
 if __name__ == '__main__':
