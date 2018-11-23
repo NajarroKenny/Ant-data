c6a8b0841559bfeed47223829795d2edc22ca134
Peter Kasprowicz <peter.kasprowicz@gmail.com>
2018-11-22 20:36:10 -0600

    Package structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ant_data',
+      version='0.1',
+      description='Fetch data from elasticsearch',
+      url='https://github.com/kingoenergy/ant_data',
+      author='Roberto Hernandez',
+      author_email='roberto.hernandez@kingoenergy.com',
+      license='Private',
+      packages=['ant_data','ant_data.codes'],
+      zip_safe=False)
