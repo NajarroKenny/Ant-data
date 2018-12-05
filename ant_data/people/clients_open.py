167153c6518d1a9ae03997ebc3302ee57ed090a5
Roberto Hernández Ch <roberto.hernandez@kingoenergy.com>
2018-12-05 15:30:14 -0600

    Column names and date fix

--- ant_data/people/clients_open.py
+++ ant_data/people/clients_open.py
@@ -39,6 +39,9 @@ def search_distinct(country, f=None, interval='month'):
   s.aggs.bucket('stats', 'children', type='stat') \
     .bucket('dates', 'date_histogram', field='date', interval=interval) \
     .metric('count', 'cardinality', field='person_id', precision_threshold=40000)
+  
+  s.post_filter('range', date={'lte': 'now'})
+  
   return s[:0].execute()
 
 
@@ -49,10 +52,10 @@ def df_start(country, f=None, interval='month'):
   merged = opened.merge(closed, on='date', how='outer').sort_index()
   merged = merged.fillna(0).astype('int64')
 
-  df_start = DataFrame(index=merged.index, columns=['open'])
+  df_start = DataFrame(index=merged.index, columns=['start'])
   
   if merged.empty:
-    return DataFrame(columns=['open'])
+    return DataFrame(columns=['start'])
 
   rev_idx = merged.index.tolist()
   rev_idx.reverse()
@@ -79,10 +82,10 @@ def df_end(country, f=None, interval='month'):
   merged = opened.merge(closed, on='date', how='outer').sort_index()
   merged = merged.fillna(0).astype('int64')
 
-  df_end = DataFrame(index=merged.index, columns=['open'])
+  df_end = DataFrame(index=merged.index, columns=['end'])
   
   if merged.empty:
-    return DataFrame(columns=['open'])
+    return DataFrame(columns=['end'])
 
   rev_idx = merged.index.tolist()
   rev_idx.reverse()
@@ -102,7 +105,7 @@ def df_end(country, f=None, interval='month'):
 def df_average(country, f=None, interval='month'):
   return DataFrame(concat(
     (df_start(country, f=f, interval=interval), df_end(country, f=f, interval=interval)), 
-    axis=1).mean(axis=1)).astype('int64')
+    axis=1).mean(axis=1)).astype('int64').rename(columns={0: 'average'})
   
 def df_distinct(country, f=None, interval='month'):
 
@@ -114,7 +117,7 @@ def df_distinct(country, f=None, interval='month'):
     obj[date.key_as_string] = { date.count.value }
 
   df = DataFrame.from_dict(
-    obj, orient='index', dtype='int64', columns=['open']
+    obj, orient='index', dtype='int64', columns=['distinct']
   )
 
   if df.empty:
@@ -134,7 +137,7 @@ def df_weighted(country, f=None, interval='month'):
     obj[date.key_as_string] = { date.doc_count }
   
   df_weighted = DataFrame.from_dict(
-    obj, orient='index', dtype='int64', columns=['open']
+    obj, orient='index', dtype='int64', columns=['weighted']
   )
 
   if df_weighted.empty:
@@ -151,22 +154,24 @@ def df_weighted(country, f=None, interval='month'):
 
 def df(country, method=None, f=None, interval='month'):
   
-  # switcher = {
-  #   'start': df_start,
-  #   'end':  df_end,
-  #   'average': df_average,
-  #   'distinct': df_distinct,
-  #   'weighted': df_weighted
-  # }
+  switcher = {
+     'start': df_start,
+     'end':  df_end,
+     'average': df_average,
+     'distinct': df_distinct,
+     'weighted': df_weighted
+   }
   
-  # return switcher.get(method)(country, f=f, interval=interval)
+  if method is not None:
+    return switcher.get(method)(country, f=f, interval=interval)
 
-  start = df_start(country, f=f, interval=interval)
-  end = df_end(country, f=f, interval=interval)
-  average = df_average(country, f=f, interval=interval)
-  distinct = df_distinct(country, f=f, interval=interval)
-  weighted = df_weighted(country, f=f, interval=interval)
+  else:
+    start = df_start(country, f=f, interval=interval)
+    end = df_end(country, f=f, interval=interval)
+    average = df_average(country, f=f, interval=interval)
+    distinct = df_distinct(country, f=f, interval=interval)
+    weighted = df_weighted(country, f=f, interval=interval)
 
-  return start.merge(end, on='date').merge(average, on='date') \
-    .merge(distinct, on='date').merge(weighted, on='date')
+    return start.merge(end, on='date').merge(average, on='date') \
+      .merge(distinct, on='date').merge(weighted, on='date')
   
