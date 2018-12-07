7e8b1877443b259adc4fc436fb8ee7d5d250b665
Roberto Hernández Ch <roberto.hernandez@kingoenergy.com>
2018-12-07 11:23:18 -0600

    Fixed and cleand installs_opened and installs_closed. Added installs_closed__model and installs_opened__model

--- ant_data/installs/installs_closed.py
+++ ant_data/installs/installs_closed.py
@@ -1,12 +1,32 @@
-from ant_data import elastic
+"""
+Systems Closed
+==========================
+Provides functions to fetch and parse data from Kingo's ElasticSearch Data
+Warehouse to generate a report on systems closed
+
+- Create date:  2018-12-04
+- Update date:  2018-12-07
+- Version:      1.1
+
+Notes:
+==========================        
+- v1.0: Initial version
+- v1.1: Updated with standard based on v1.1 of systems_closed__model
+"""
 from elasticsearch_dsl import Search, Q
 from pandas import DataFrame, Series
+
+from ant_data import elastic
 
 
 def search(country, f=None, interval='month'):
   s = Search(using=elastic, index='installs') \
-    .query('bool', filter=[Q('term', country=country), 
-                           Q('term', doctype='install')])
+    .query(
+      'bool', filter=[
+        Q('term', country=country), Q('term', doctype='install'),
+        Q('term', system_type='kingo'), Q('range', closed={'lte': 'now'})
+      ]
+    )
 
   if f is not None:
     s = s.query('bool', filter=f)
@@ -18,12 +38,14 @@ def search(country, f=None, interval='month'):
 
   return s[:0].execute()
 
+
 def df(country, f=None, interval='month'):
   response = search(country, f=f, interval=interval)
 
-  obj = {}
+  dates = [x.key_as_string for x in response.aggs.dates.buckets]
+  obj = {x: { 0 } for x in dates}
 
-  for date in response.aggregations.dates.buckets: 
+  for date in response.aggs.dates.buckets: 
     obj[date.key_as_string] = { date.doc_count }
 
   df = DataFrame.from_dict(
