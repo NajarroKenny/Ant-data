824cfa7e9d0100ad431fb3ba444a0b5b90f3aa27
Roberto Hernández Ch <roberto.hernandez@kingoenergy.com>
2018-12-10 14:55:32 -0600

    NaN agent_id bug and - person_id bug fixes

--- ant_data/scripts/sync_log.py
+++ ant_data/scripts/sync_log.py
@@ -24,24 +24,32 @@ from sqlalchemy import create_engine
 from ant_data import elastic
 
 
-# CWD = os.path.dirname(os.path.realpath(__file__))
-# sys.path.append(CWD)
-
 config = configparser.ConfigParser()
 config.read(sys.path[-1]+'/config.ini')
 
 
-def fetch_sync_log():
+def engine():
+  return create_engine(
+    f"postgresql+psycopg2://{config['PG']['USER']}:{config['PG']['PASSWORD']}"
+    f"@{config['PG']['HOST']}:{config['PG']['PORT']}/{config['PG']['GTM-DB']}", 
+    echo=True
+  )
+
+
+ENGINE = engine()
+
+
+def fetch_sync_log(engine):
   query = (
-    "SELECT (sl.region||':pos:'||sl.pos_id) AS pos_id, "
+    "SELECT (sl.region||':pos:'||sl.pos_id) AS system_id, "
       "CASE "
         "WHEN ai.application_instance_type = 1 THEN 'shopkeeper' "
         "WHEN ai.application_instance_type = 2 THEN 'employee' "
         "WHEN ai.application_instance_type = 3 THEN 'distributor' "
         "WHEN ai.application_instance_type IS NULL THEN NULL "
       "END AS instance_type, "
-      "ai.shopkeeper_id shopkeeper_id, "
-      "ai.employee_id employee_id, "
+      "ai.shopkeeper_id AS person_id, "
+      "ai.employee_id AS employee_id, "
       "max(sl.date) sync_date "
     "FROM public.sync_log sl "
     "INNER JOIN stream.application_instance ai ON ai.id = sl.pos_id "
@@ -51,40 +59,47 @@ def fetch_sync_log():
   
   print(f'Query to be run is {query} \n')
 
-  engine = create_engine(
-    f"postgresql+psycopg2://{config['PG']['USER']}:{config['PG']['PASSWORD']}"
-    f"@{config['PG']['HOST']}:{config['PG']['PORT']}/{config['PG']['GTM-DB']}", 
-    echo=True
-  )
-
   print(f'{Timestamp.now().isoformat()} - Starting to fetch sync log')
-  sync_log = read_sql(query, engine, index_col='pos_id')
+  sync_log = read_sql(query, engine, index_col='system_id')
   sync_log['sync_date'] = sync_log['sync_date'].apply(lambda x: x.isoformat())
   print(f'{Timestamp.now().isoformat()} - Finished fetching sync log')
 
   return sync_log
 
+
+def fetch_user_list(engine):
+  query = (
+    "SELECT id AS employee_id, username AS agent_id "
+    "FROM auth.user_account "
+    "ORDER BY 1;"
+  )
+  
+  print(f'Query to be run is {query} \n')
+  
+  return read_sql(query, engine)
+
+
 def index_sync_log():
-  def gendata(sync_log):
-    for i in range(len(sync_log)):
-      pos_id = sync_log.index[i]
-      instance_type = sync_log.iloc[i]['instance_type'] 
-      shopkeeper_id = sync_log.iloc[i]['shopkeeper_id']
-      employee_id = sync_log.iloc[i]['employee_id']
-      sync_date = sync_log.iloc[i]['sync_date']  
+  def gendata(df):
+    for i in range(len(df)):
+      system_id = df.index[i]
+      instance_type = df.iloc[i]['instance_type'] 
+      person_id = df.iloc[i]['person_id']
+      agent_id = df.iloc[i]['agent_id']
+      sync_date = df.iloc[i]['sync_date']  
       
       doc = {
-        "pos_id": pos_id,
+        "system_id": system_id,
         "sync_date": sync_date}
 
       if instance_type is not None:
         doc['instance_type'] = instance_type
       
-      if shopkeeper_id is not None:
-        doc['shopkeeper_id'] = shopkeeper_id
+      if person_id is not None:
+        doc['person_id'] = str(person_id).replace('-','')
 
-      if employee_id is not None:
-        doc['employee_id'] = employee_id
+      if isinstance(agent_id, str):
+        doc['agent_id'] = agent_id
 
       yield {
           "_index": "sync_log",
@@ -92,9 +107,12 @@ def index_sync_log():
           "doc": doc
       }
   
-  sync_log = fetch_sync_log()
+  sync_log = fetch_sync_log(ENGINE)
+  user_list = fetch_user_list(ENGINE)
+  sync_df = sync_log.merge(user_list, on='employee_id', how='left')\
+    .drop('employee_id', axis=1).set_index(sync_log.index)
   
   print(f'{Timestamp.now().isoformat()} - Starting to index sync log')
-  bulk(elastic, gendata(sync_log))
+  bulk(elastic, gendata(sync_df))
   print(f'{Timestamp.now().isoformat()} - Finished indexind sync log')
 
