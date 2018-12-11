2cfdb75e3963f36f229606b38a433ecdbfac73b8
Roberto Hernández Ch <roberto.hernandez@kingoenergy.com>
2018-12-11 16:00:51 -0600

    Fixed index_sync_log and added sync_log. Added COUNTRY_LIST to GEOGRAPHY and TZ info to TIME

---
+++ ant_data/people/sync_log.py
@@ -0,0 +1,81 @@
+"""
+Sync log
+==========================
+Fetches the sync log stored in Elasticsearch
+
+- Create date:  2018-12-11
+- Update date:  2018-12-11
+- Version:      1.0
+
+Notes:
+==========================        
+- v1.0: Initial version
+"""
+from elasticsearch_dsl import Search, Q
+from pandas import DataFrame, Series
+
+from ant_data import elastic
+from ant_data.static.GEOGRAPHY import COUNTRY_LIST
+
+
+def search(country, f=None):
+  if country not in COUNTRY_LIST:
+    raise Exception(f'{country} is not a valid country')
+
+  s = Search(using=elastic, index='sync_log') \
+    .query(
+      'bool', filter= Q('term', country=country)
+    )
+
+  if f is not None:
+    s = s.query('bool', filter=f)
+
+  return s.scan()
+
+
+def df(country, f=None):
+  if country not in COUNTRY_LIST:
+    raise Exception(f'{country} is not a valid country')
+  
+  response = search(country, f=f)
+
+  obj = {}
+
+  for hit in response:
+    try:
+      person_id = hit.person_id
+    except:
+      person_id = None
+
+    try:
+      agent_id = hit.agent_id
+    except:
+      agent_id = None
+
+    try:
+      instance_type = hit.instance_type
+    except:
+      instance_type = None
+
+    obj[hit.system_id] = { 
+      'country': hit.country,
+      'instance_type': instance_type,
+      'agent_id': agent_id,
+      'person_id': person_id,
+      'sync_date': hit.sync_date
+      }
+
+  df = DataFrame.from_dict(
+    obj, orient='index', columns=[
+      'country','instance_type', 'agent_id', 'person_id', 'sync_date'
+    ]
+  )
+  
+  if df.empty:
+    return df
+
+  df.index.name = 'system_id'
+  df = df.sort_index()
+
+  return df
+
