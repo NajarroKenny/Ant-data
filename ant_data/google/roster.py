1cc7a231468685db66ad16731f70c7efcb0fac38
Roberto Hernández Ch <roberto.hernandez@kingoenergy.com>
2018-12-14 16:59:32 -0600

    Split between AT & ATR. Using temp roster with emails.

--- ant_data/google/roster.py
+++ ant_data/google/roster.py
@@ -22,10 +22,13 @@ from pandas import DataFrame
 # If modifying these scopes, delete the file token.json.
 SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
 # The ID and range of a sample spreadsheet.
-SPREADSHEET_ID = '1UjLlBMpf7UqOWe2Icp3kJuOHc0zN0S_U365o4-AnMQw'
-RANGE1 = 'AT´S!B2:K'
-RANGE2 = 'ATR´S!B2:K'
-FNAME = 'roster.csv'
+# SPREADSHEET_ID = '1UjLlBMpf7UqOWe2Icp3kJuOHc0zN0S_U365o4-AnMQw' FIXME:
+SPREADSHEET_ID = '1WXLG241suRmBKKGJyyu43-T5DYErPrpr0GBG0T8ogq0'
+RANGE_AT = 'AT´S!B2:M'
+RANGE_ATR = 'ATR´S!B2:M'
+
+FNAME_AT = 'roster_at.csv'
+FNAME_ATR = 'roster_atr.csv'
 
 
 def main():
@@ -41,30 +44,33 @@ def main():
 
     # Call the Sheets API
     sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
-                                range=RANGE1).execute()
-    values = result.get('values', [])
+    
+    result_at = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
+                                range=RANGE_AT).execute()
+    values_at = result_at.get('values', [])
 
-    df = DataFrame(
-        values, columns=[
-            'nombre', 'puesto', 'id', 'cs', 'ss', 'departamento', 'municipio', 
-            'telefono', 'correo', 'ingreso'
+    df_at = DataFrame(
+        values_at, columns=[
+            'nombre', 'puesto', 'id', 'cs', 'correo_cs', 'ss', 'correo_ss', 
+            'departamento', 'municipio', 'telefono', 'correo', 'ingreso'
             ]
         )
+    df_at = df_at.drop(df_at[df_at['nombre']=='Vacante'].index)
 
-    result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
-                                range=RANGE2).execute()
-    values = result.get('values', [])
-    df = df.append(DataFrame(
-        values, columns=[
-            'nombre', 'puesto', 'id', 'cs', 'ss', 'departamento', 'municipio', 
-            'telefono', 'correo', 'ingreso'
+    result_atr = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
+                                range=RANGE_ATR).execute()
+    values_atr = result_atr.get('values', [])
+    
+    df_atr = DataFrame(
+        values_atr, columns=[
+            'nombre', 'puesto', 'id', 'cs', 'correo_cs', 'ss', 'correo_ss', 
+            'departamento', 'municipio', 'telefono', 'correo', 'ingreso'
             ]
-        ))
-
-    df = df.drop(df[df['nombre']=='Vacante'].index)
-
-    df.to_csv(FNAME, sep=',')
+        )
+    df_atr = df_atr.drop(df_atr[df_atr['nombre']=='Vacante'].index)
+    
+    df_at.to_csv(FNAME_AT, sep=',')
+    df_atr.to_csv(FNAME_ATR, sep=',')
 
 if __name__ == '__main__':
     main()
