3e8c8f4f538dfaca7a41d3e7de2e26f1c303083e
Roberto Hernández Ch <roberto.hernandez@kingoenergy.com>
2018-12-14 17:55:41 -0600

    First version of people/roster.py. Added roster functions to people/people.py

--- ant_data/people/people.py
+++ ant_data/people/people.py
@@ -4,6 +4,7 @@ from ant_data.people import people_closed as _people_closed
 from ant_data.people import people_open as _people_open
 from ant_data.people import days as _days
 from ant_data.people import days__cohort as _days__cohort
+from ant_data.people import roster as _roster
 
 def people_opened(country, f=None, interval='month'):
   return _people_opened.df(country, f=f, interval=interval)
@@ -109,3 +110,9 @@ def no_persons_days__cohort(country, f=None, interval='month'):
     f = []
   f.append(Q('term', doctype='no_person'))
   return _days__cohort.df(country, f=f, interval=interval)
+
+def roster_at():
+  return _roster.at()
+
+def roster_atr():
+  return _roster.atr()
---
+++ ant_data/people/roster.py
@@ -0,0 +1,34 @@
+"""
+Roster
+============================
+Loads the AT & ATR Roster CSV files into Pandas DataFrames
+
+- Create date:  2018-12-14
+- Update date:  2018-12-14
+- Version:      1.0
+
+Notes:
+============================      
+- v1.0: Initial version
+"""
+import pkg_resources
+
+import pandas as pd
+from pandas import DataFrame
+
+
+def at():
+  path = '../google/roster_at.csv'
+  filepath = pkg_resources.resource_filename(__name__, path)
+  at = pd.read_csv(filepath, index_col='correo')
+  at.index.name = 'agent_id'
+
+  return at
+
+def atr():
+  path = '../google/roster_atr.csv'
+  filepath = pkg_resources.resource_filename(__name__, path)
+  atr = pd.read_csv(filepath, index_col='correo')
+  atr.index.name = 'agent_id'
+
+  return atr
