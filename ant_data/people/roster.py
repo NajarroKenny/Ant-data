692e6204a36e7503060e24891869a4cd19bffe3e
Peter Kasprowicz <peter.kasprowicz@gmail.com>
2018-12-15 16:35:57 -0600

    roster person function

--- ant_data/people/roster.py
+++ ant_data/people/roster.py
@@ -8,7 +8,7 @@ Loads the AT & ATR Roster CSV files into Pandas DataFrames
 - Version:      1.0
 
 Notes:
-============================      
+============================
 - v1.0: Initial version
 """
 import pkg_resources
@@ -20,7 +20,7 @@ from pandas import concat,DataFrame
 def agents():
   path = '../google/roster_agents.csv'
   filepath = pkg_resources.resource_filename(__name__, path)
-  
+
   return pd.read_csv(filepath, index_col='agent_id')
 
 def supervisors():
@@ -32,5 +32,44 @@ def supervisors():
 def coordinators():
   path = '../google/roster_coordinators.csv'
   filepath = pkg_resources.resource_filename(__name__, path)
-  
+
   return pd.read_csv(filepath, index_col='coordinator_id')
+
+def person(id):
+  path = '../google/roster_agents.csv'
+  filepath = pkg_resources.resource_filename(__name__, path)
+  agents = pd.read_csv(filepath, index_col='agent_id')
+
+  path = '../google/roster_coordinators.csv'
+  filepath = pkg_resources.resource_filename(__name__, path)
+  coordinators = pd.read_csv(filepath, index_col='coordinator_id')
+
+  path = '../google/roster_supervisors.csv'
+  filepath = pkg_resources.resource_filename(__name__, path)
+  supervisors = pd.read_csv(filepath, index_col='supervisor_id')
+
+  level = ''
+  if id in agents.index:
+    level = 'agent'
+    agent = agents.loc[id]
+    coordinator = agent['coordinator_id']
+    supervisor = agent['supervisor_id']
+  elif id in coordinators.index:
+    level = 'coordinator'
+    coordinator = coordinators.loc[id]
+    supervisor = coordinator['supervisor_id']
+    agent = agents[agents['coordinator_id'] == id].index.values
+  elif id in supervisors.index:
+    level = 'supervisor'
+    supervisor = supervisors.loc[id]
+    agent = agents[agents['supervisor_id'] == id].index.values
+    coordinator = coordinators[coordinators['supervisor_id'] == id].index.values
+  else:
+    return None
+
+  return {
+    'level': level,
+    'agent': agent,
+    'coordinator': coordinator,
+    'supervisor': supervisor
+  }
