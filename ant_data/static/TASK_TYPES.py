a9a32a77b3735bb133449ad7f4a57ab0723a45fa
Roberto Hernández Ch <roberto.hernandez@kingoenergy.com>
2018-12-20 08:35:27 -0600

    Tasks by types and effective

--- ant_data/static/TASK_TYPES.py
+++ ant_data/static/TASK_TYPES.py
@@ -7,3 +7,30 @@ TASK_TYPES = {
   'sk-follow-up': 'Seguimiento Tendero',
   'total': 'Total'
 }
+
+ADDITIONAL_TASK_TYPES = {
+  'active-code': 'Saldo activo',
+  'install': 'Instalación',
+  'pickup': 'Recogido',
+  'register': 'Registro',
+  'sale': 'Venta',
+  'shopkeeper-pickup': 'Tendero recogido',
+  'shopkeeper-sync': 'Sincronización de tendero',
+  'shopkeeper-visit': 'Visita a tendero',
+  'swap': 'Cambio',
+  'visit-install': 'visit-install'
+}
+
+SALE_VALUES = {
+  'code-hour': 0,
+  'code-day': 1,
+  'code-week': 7,
+  'code-fortnight': 14,
+  'code-month': 30,
+  'code-quarter': 90,
+  'code-semester': 180,
+  'code-year': 365,
+  'receipt-day': 1,
+  'receipt-week': 7,
+  'receipt-month': 132
+}
