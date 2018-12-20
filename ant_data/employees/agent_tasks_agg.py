dd0ce4d2d0c6f1229775c1eef7f2b1a587e22978
Peter Kasprowicz <peter.kasprowicz@gmail.com>
2018-12-20 11:21:33 -0600

    Tasks stuff

---
+++ ant_data/employees/agent_tasks_agg.py
@@ -0,0 +1,99 @@
+"""
+CS Tasks
+==========================
+Provides functions to fetch and parse data from Kingo's ElasticSearch Data
+Warehouse to generate a report on CS tasks.
+
+- Create date:  2018-12-18
+- Update date:  2018-12-18
+- Version:      1.0
+
+Notes:
+==========================
+- v1.0: Initial version
+"""
+from elasticsearch_dsl import Q
+import numpy as np
+from pandas import DataFrame
+
+from ant_data import elastic
+from ant_data.employees import hierarchy
+from ant_data.static.AGENT_MAPPING import AGENT_MAPPING
+from ant_data.static.GEOGRAPHY import COUNTRY_LIST
+from ant_data.static.TASK_TYPES import TASK_TYPES
+from ant_data.static.TIME import TZ
+from ant_data.tasks.tasks import *
+
+
+VISITED_F = [Q('has_child', type='history', query=Q())]
+ADDITIONAL_F = [Q('term', planned=False)]
+
+
+def data(country, coordinator_id, start, end, f=None):
+  if country not in COUNTRY_LIST:
+    raise Exception(f'{country} is not a valid country')
+
+  info = hierarchy.info(coordinator_id)
+  if info is None:
+    return DataFrame()
+
+  agent_list = info['agent_id']
+
+  if agent_list==[]:
+    return DataFrame()
+
+  if f is None:
+    f = []
+
+  f += [Q('terms', agent_id = agent_list)]
+
+  df_tasks = tasks__types(country, start=start, end=end, f=f, interval='year').sum(axis=0)
+  df_tasks = DataFrame(df_tasks, columns=['tasks'])
+  df_tasks.index.name = 'types'
+
+  if df_tasks.empty:
+    return df_tasks
+
+  df_tasks = tasks__types(country, start=start, end=end, f=f, interval='year').sum(axis=0)
+  df_tasks = DataFrame(df_tasks, columns=['tasks'])
+  df_tasks.index.name = 'types'
+
+  if df_tasks.empty:
+    return df_tasks
+
+  df_visited = tasks__types(country, start=start, end=end, f=f+VISITED_F, interval='year')
+  df_visited = DataFrame(df_visited.sum(axis=0), columns=['visited'])
+  df_visited.index.name = 'types'
+  df_effective = tasks_effective__types(country, start=start, end=end, f=f+VISITED_F, interval='year')
+  df_effective = DataFrame(df_effective.sum(axis=0), columns=['effective'])
+  df_effective.index.name = 'types'
+  df_additional = tasks__types(country, start=start, end=end, f=f+ADDITIONAL_F, interval='year')
+  df_additional = DataFrame(df_additional.sum(axis=0), columns=['additional'])
+  df_additional.index.name = 'types'
+
+  df = df_tasks.merge(df_visited, on='types', how='left')
+  df = df.merge(df_effective, on='types', how='left')
+  df = df.merge(df_additional, on='types', how='left')
+  df = df.fillna(0).astype('int64')
+  df['% visitadas'] = df['visited'].div(df['tasks'])
+  df['% efectivas'] = df['effective'].div(df['visited'])
+  df = df.fillna(0).replace((np.inf, -np.inf), (0,0))
+  df = df.replace(np.nan, 0)
+  df.index.name = 'Tipo de tarea'
+
+  df = df.rename(
+    index=TASK_TYPES, 
+    columns= {
+      'tasks': 'Tareas Asignadas',
+      'visited': 'Asignadas Visitadas',
+      'effective': 'Visitas Efectivas',
+      'additional': 'Tareas Adicionales'
+    }
+  )
+
+  df = df[[
+    'Tareas Asignadas', 'Asignadas Visitadas', '% visitadas', 
+    'Visitas Efectivas', '% efectivas', 'Tareas Adicionales'
+  ]]
+
+  return df
---
+++ ant_data/tasks/assigned_tasks__agent.py
@@ -0,0 +1,110 @@
+"""
+Task Types
+============================
+Provides functions to fetch and parse data from Kingo's ElasticSearch Data
+Warehouse to generate a report on task types.
+
+- Create date:  2018-12-19
+- Update date:
+- Version:      1.0
+
+Notes:
+============================
+- v1.0: Initial version
+"""
+from elasticsearch_dsl import Search, Q
+from pandas import DataFrame, Series
+
+from ant_data import elastic
+
+
+def search(agent_id=None, start=None, end=None, f=None):
+    s = Search(using=elastic, index='tasks') \
+        .query('term', doctype='task') \
+        .query('term', planned=True)
+    if agent_id is not None:
+        agent_list = agent_id if isinstance(agent_id, list) else [agent_id]
+        s = s.query('terms' , agent_id=agent_list)
+    if start is not None:
+        s = s.query('bool', filter=Q('range', due={ 'gte': start }))
+    if end is not None:
+        s = s.query('bool', filter=Q('range', due={ 'lt': end }))
+    if f is not None:
+        s = s.query('bool', filter=f)
+
+    s.aggs.bucket('agents', 'terms' field='agent_id', size=10000, min_doc_count=1) \
+        .bucket('remarks', 'terms', field='remarks', size=10000, min_doc_count=1)
+
+    return s[:0].execute()
+
+
+def df(start=None, end=None, f=None, workflow=None, all=False):
+    """Assigned tasks, by type"""
+    g = [] if f is None else f[:]
+
+    if workflow is not None:
+        g.append(Q('has_child', type='history', query=Q('terms', workflow=workflow)))
+
+    response = search(start=start, end=end, f=g)
+
+    obj = {}
+
+    for remark in response.aggs.remarks.buckets:
+        if (
+            remark.key.startswith('gestion') or
+            remark.key.startswith('gestion 2') or
+            remark.key.startswith('gestion 3')
+        ):
+            tipo = 'Gestión'
+        elif remark.key.startswith('ticket'):
+            tipo = 'Ticket'
+        elif remark.key.startswith('verificacion'):
+            tipo = 'Verificación'
+        elif remark.key.startswith('tendero venta'):
+            tipo = 'Tendero Venta'
+        elif remark.key.startswith('tendero sync'):
+            tipo = 'Tendero Sync'
+        elif remark.key.startswith('kingo basico'):
+            tipo = 'Swap K7 o Venta >= 1 semana'
+        elif remark.key.startswith('kingo tv'):
+            tipo = 'Swap a Kingo TV'
+        elif (
+            remark.key.startswith('tecnica') or
+            remark.key.startswith('promocion precio') or
+            remark.key.startswith('corregir') or
+            remark.key.startswith('cliente activo') or
+            remark.key.startswith('asignacion especial tecnica')
+        ):
+            tipo = 'Técnica'
+        elif remark.key.startswith('instalacion k7'):
+            tipo = 'Instalación Kingo Básico'
+        elif remark.key.startswith('instalacion k15'):
+            tipo = 'Instalación Kingo Luz'
+        elif remark.key.startswith('instalacion ktv'):
+            tipo = 'Instalación Kingo TV'
+        elif remark.key.startswith('instalacion k100+'):
+            tipo = 'Instalación Kingo Hogar'
+        elif (
+            remark.key.startswith('preventa kingo tv') or
+            remark.key.startswith('preventa: k15 tv') or
+            remark.key.startswith('preventa: kingo tv')
+        ):
+            tipo = 'Preventa Kingo TV'
+        elif remark.key.startswith('preventa kingo hogar'):
+            tipo = 'Preventa Kingo Hogar'
+        else:
+            tipo = 'Sin Tipo'
+        obj[tipo] = obj.get(tipo, 0) + remark.doc_count
+
+    df = DataFrame.from_dict(obj, orient='index', columns=['Asignadas'])
+
+    if df.empty:
+        return df
+
+    if not all and 'Sin Tipo' in df.index:
+        df = df.drop('Sin Tipo')
+
+    df.index.name = 'Tipo de Tarea'
+    df.loc['Total'] = df.sum()
+
+    return df
