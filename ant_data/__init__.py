9a4e30e799fb2d77e9c9815463ef6c832d7c4959
Roberto Hernández Ch <roberto.hernandez@kingoenergy.com>
2018-12-26 15:06:27 -0600

    Document and clean ant_data init file

--- ant_data/__init__.py
+++ ant_data/__init__.py
@@ -1,28 +1,51 @@
+
+"""
+Ant Data Init File
+==========================
+Indexes roster and community master information in hierarchy index
+
+- Create date:  2018-11-28
+- Update date:  2018-12-26
+- Version:      1.2
+
+Notes:
+==========================
+- v1.0: Initial version
+- v1.1: 
+- v1.2: Renamed config to uppercase. Added case where timeout might not be
+        existent
+"""
 import configparser
 import os
 import sys
 
 from elasticsearch import Elasticsearch
 
-CWD = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(CWD)
 
-config = configparser.ConfigParser()
-config.read(sys.path[-1]+'/config.ini')
+ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(ROOT_DIR)
 
-if config['ES']['HOST'] != '' and config['ES']['PORT'] != '':
-    print(f"Elasticsearch host is {config['ES']['HOST']}")
-    print(f"Elasticsearch port is {config['ES']['PORT']}")
-    print(f"Elasticsearch timeout is {config['ES']['TIMEOUT']}")
+CONFIG = configparser.ConfigParser()
+CONFIG.read(sys.path[-1]+'/config.ini')
 
-    elastic = Elasticsearch(
-        config['ES']['HOST'],
-        port= config['ES']['PORT'],
-        timeout = float(config['ES']['TIMEOUT'])
-    )
+if CONFIG['ES']['HOST'] != '' and CONFIG['ES']['PORT'] != '':
+  print(f"Elasticsearch host is {CONFIG['ES']['HOST']}")
+  print(f"Elasticsearch port is {CONFIG['ES']['PORT']}")
+  print(f"Elasticsearch timeout is {CONFIG['ES']['TIMEOUT']}")
+
+  elastic = Elasticsearch(
+    CONFIG['ES']['HOST'],
+    port= CONFIG['ES']['PORT'],
+    timeout = float(CONFIG['ES']['TIMEOUT'])
+  )
+
+elif CONFIG['ES']['HOST'] != '':
+  print(f"Elasticsearch timeout is {CONFIG['ES']['TIMEOUT']}")
+  elastic = Elasticsearch(timeout = float(CONFIG['ES']['TIMEOUT']))
 
 else:
-    print(f"Elasticsearch timeout is {config['ES']['TIMEOUT']}")
-    elastic = Elasticsearch(
-        timeout = float(config['ES']['TIMEOUT'])
+    print(
+       f'No configuration parameters passed to Elasticsearch. Instanting '
+       f'client with default parameters.'
     )
+    elastic = Elasticsearch()
