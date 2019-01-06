304133e37ae363daeadeb55d865cc5f0ef9506f9
Peter Kasprowicz <peter.kasprowicz@gmail.com>
2019-01-06 15:25:21 -0600

    control_board -> arpu

--- ant_data/combo/arpu.py
+++ ant_data/combo/arpu.py
@@ -6,31 +6,34 @@ from ant_data.stats import active__date as ad
 from ant_data.stats import dev_active_systems__date as asd
 from ant_data.people import people as p, dev_clients_open_from_installs as dev_clients_open
 from ant_data.installs import installs as i
-from ant_data.codes import codes as c
+from ant_data.codes import codes_to_kingos, codes as c
 
 def df(country, start=None, end=None, f=None, interval='month'):
   col1 = dev_clients_open.df(country, start, end)
   col2 = p.people_open(country, method='weighted', start=start, end=end, f=f, interval=interval)
-  col3 = asd.df(country, start=start, end=end, paid=False, f=f, interval=interval)
-  col4 = asd.df(country, start=start, end=end, paid=True, f=f, interval=interval)
-  col5 = i.kingos_open(country, start=start, end=end, f=f, interval=interval)['total']
-  col6 = c.df(country, doctype='code', start=start, end=end, f=f, interval=interval)['paid']
+  col3 = asd.df(country, start=start, end=end, f=f, interval=interval)
+  col4 = i.kingos_open(country, start=start, end=end, f=f, interval=interval)['total']
+  col5 = c.df(country, doctype='code', start=start, end=end, f=f, interval=interval)['paid']
+  col6 = codes_to_kingos.df(country, start=start, end=end, f=f, interval=interval)
+  col6['buying_kingos'] = col6['parents.paid'] + col6['orphan.paid']
+  col6 = col6[['buying_kingos']]
 
   df = join_df('date', 'outer', col1, col2, col3, col4, col5, col6)
   df = df.sort_index().fillna(0)
   df = df.rename(columns={
     'end': 'clientes',
-    'weighted': 'kingos (ponderado)',
-    'active_systems_x': 'kingos activos',
-    'active_systems_y': 'kingos activos pagado',
     'total': 'kingos',
-    'paid': 'ingresos de clientes',
-    'no_model': 'sin modelo'
+    'weighted': 'kingos (ponderado)',
+    'active_systems': 'kingos activos',
+    'buying_kingos': 'kingos pagados',
+    'paid': 'ingresos de clientes'
   })
   # df = df.rename(columns={x: x.lower() for x in col5.columns})
-  df['% activos pagado'] = df['kingos activos pagado'] / df['kingos']
   df['% activos'] = df['kingos activos'] / df['kingos']
+  df['% pagados'] = df['kingos pagados'] / df['kingos']
   df['arpu'] = df['ingresos de clientes'] / df['kingos (ponderado)']
+  df['arpu activo'] = df['ingresos de clientes'] / df['kingos activos']
+  df['arpu pagado'] = df['ingresos de clientes'] / df['kingos pagados']
 
   df = df.replace((-np.inf, np.inf, np.nan), (0,0,0))
 
@@ -39,18 +42,18 @@ def df(country, start=None, end=None, f=None, interval='month'):
     'kingos': 'int64',
     'kingos (ponderado)': 'int64',
     'kingos activos': 'int64',
-    'kingos activos pagado': 'int64'
+    'kingos pagados': 'int64',
   })
 
-  df[['% activos pagado', '% activos']] = df[['% activos pagado', '% activos']]*100
-  df['arpu activo'] = df['ingresos de clientes'] / df['kingos activos']
+  df[['% pagados', '% activos']] = df[['% pagados', '% activos']]*100
 
   df = df.round({
-    'ingresos de cliente': 2,
-    '% activos pagado': 1,
+    'ingresos de clientes': 2,
     '% activos': 1,
+    '% pagados': 1,
     'arpu': 2,
-    'arpu activo': 2
+    'arpu activo': 2,
+    'arpu pagado': 2
   })
 
   df = df[[
@@ -59,11 +62,12 @@ def df(country, start=None, end=None, f=None, interval='month'):
     'kingos (ponderado)',
     'kingos activos',
     '% activos',
-    'kingos activos pagado',
-    '% activos pagado',
+    'kingos pagados',
+    '% pagados',
     'ingresos de clientes',
     'arpu',
-    'arpu activo'
+    'arpu activo',
+    'arpu pagado'
   ]]
 
 
