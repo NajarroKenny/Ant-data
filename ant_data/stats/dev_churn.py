aeae49dd1d861e12ed6528d176a8a0c33e1f8803
Peter Kasprowicz <peter.kasprowicz@gmail.com>
2019-01-07 06:00:25 -0600

    Tidy up of dev_churn

--- ant_data/stats/dev_churn.py
+++ ant_data/stats/dev_churn.py
@@ -7,40 +7,61 @@ from pandas import DataFrame, date_range
 
 from ant_data import elastic, ROOT_DIR
 from ant_data.people import people_open
-from ant_data.stats import dev_active_systems__date
+from ant_data.stats import dev_active_systems__date, dev_cohort_installs
 
 
 CONFIG = configparser.ConfigParser()
 CONFIG.read(ROOT_DIR + '/config.ini')
 
-def df(country, start, end, f=None, paid=False, interval='month'):
+def df(country, start, end, f=None, paid=False, interval='month', model=None, version=None, model_version=None):
   g = [] if f is None else deepcopy(f)
-  
+
   BUCKETS = date_range(start, end, freq='MS')
   N = len(BUCKETS)
-  
-  df = DataFrame(columns=['cohorte', 'kingos ponderados', 'kingos activos', '% kingos activos'])
-  df.index.name = 'date' 
+
+  df = DataFrame(columns=['cohort', 'weighted', 'active_systems'])
+  df.index.name = 'date'
 
   for i in range(N-1):
     cohort = BUCKETS[i].date().isoformat()
-    cohort_filter = [Q('range', kingo_opened = {'gte': BUCKETS[i], 'lt': BUCKETS[i+1]})] 
-    open_systems = people_open.df(country, method='weighted', start=cohort, end=end, f=g+cohort_filter, interval=interval)
-    active_systems = dev_active_systems__date.df(country, start=cohort, end=end, f=g+cohort_filter, paid=False, interval=interval)
+    cohort_filter = [Q('range', kingo_opened = {'gte': BUCKETS[i], 'lt': BUCKETS[i+1]})]
+
+    open_systems = people_open.df_weighted(country, start=cohort, end=end, f=g+cohort_filter, interval=interval, model=model, version=version, model_version=model_version)
+
+    active_systems = dev_active_systems__date.df(country, start=cohort, end=end, f=g+cohort_filter, interval=interval, model=model, version=version, model_version=model_version)
+
     df_cohort = open_systems.merge(active_systems, on='date', how='inner')
-    df_cohort = df_cohort.rename(columns={
-      'weighted': 'kingos ponderados',
-      'active_systems': 'kingos activos'
-    })
-    df_cohort['% kingos activos'] = df_cohort['kingos activos'] / df_cohort['kingos ponderados']
-    df = df.replace((-np.inf, np.inf, np.nan), (0,0,0))
-    df_cohort['cohorte'] = cohort
-    df_cohort = df_cohort[['cohorte', 'kingos ponderados', 'kingos activos', '% kingos activos']]
+
+    df_cohort['cohort'] = cohort
+    df_cohort = df_cohort[['cohort', 'weighted', 'active_systems']]
     df = df.append(df_cohort)
+
+  ci = dev_cohort_installs.df('Guatemala', start, end)
+  ci.index.name = 'cohort'
+  ci = ci.reset_index()
+  ci = ci[['cohort', 'kingos']]
+  ci = ci.rename(columns={ 'kingos': 'installed' })
+
+  df = df.reset_index()
+  df = df.merge(ci, on='cohort', how='inner')
+  df = df.set_index('date')
+
+  df['% active'] = df['active_systems'] / df['installed']
+  df['% weighted'] = df['weighted'] / df['installed']
+  df = df.replace((-np.inf, np.inf, np.nan), (0,0,0))
+
+  df = df[['cohort', 'installed', 'weighted', '% weighted', 'active_systems', '% active']]
+  df = df.rename(columns={
+      'cohort': 'cohorte',
+      'installed': 'instalados',
+      'weighted': 'ponderados',
+      '% weighted': '% ponderados',
+      'active_systems': 'activos',
+      '% active': '% activos'
+    })
 
   return df
 
-    
 
 
-  
+
